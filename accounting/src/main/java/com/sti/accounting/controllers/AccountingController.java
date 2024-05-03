f1b498c364b92c5d6b6f4f96afaf9dd5ec84f82e
Jose Manuel Ramos Morel <jramos@stiglobals.com>
2024-05-03 11:59:03 -0600

    action

--- accounting/src/main/java/com/sti/accounting/controllers/AccountingController.java
+++ accounting/src/main/java/com/sti/accounting/controllers/AccountingController.java
@@ -17,6 +17,7 @@ import java.util.List;
 @RequestMapping("/api/v1/accounts")
 public class AccountingController {
 
+
     private final AccountService accountService;
     private final Util util;
 
