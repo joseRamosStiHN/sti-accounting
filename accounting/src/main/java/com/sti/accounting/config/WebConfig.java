4bc43e9c8a829c242d26b814a27f35612ed3ab5f
Jose Manuel Ramos Morel <jramos@stiglobals.com>
2024-05-08 15:39:27 -0600

    add class to handle cors

--- accounting/src/main/java/com/sti/accounting/config/WebConfig.java
+++ accounting/src/main/java/com/sti/accounting/config/WebConfig.java
@@ -1,15 +1,25 @@
 package com.sti.accounting.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig  {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("*").allowedOrigins("*");
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*"); // Aceptar todos los orígenes (ajustar para producción)
+        config.addAllowedHeader("*"); // Permite cualquier encabezado
+        config.addAllowedMethod("*"); // Permite todos los métodos HTTP
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
 
