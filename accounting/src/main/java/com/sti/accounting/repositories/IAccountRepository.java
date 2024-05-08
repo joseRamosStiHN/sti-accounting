ae0725bc2c54a005574bff7c312567c419e40c90
Jose Manuel Ramos Morel <jramos@stiglobals.com>
2024-05-08 15:43:53 -0600

    add method to IAccountRepository interface

--- accounting/src/main/java/com/sti/accounting/repositories/IAccountRepository.java
+++ accounting/src/main/java/com/sti/accounting/repositories/IAccountRepository.java
@@ -2,7 +2,9 @@ package com.sti.accounting.repositories;
 
 import com.sti.accounting.entities.AccountEntity;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,4 +12,8 @@ public interface IAccountRepository extends ListCrudRepository<AccountEntity, Lo
 
     boolean existsByCode(String code);
 
+    @Query("SELECT CASE WHEN COUNT(a) > 0 THEN true ELSE false END " +
+            "FROM AccountEntity a " +
+            "WHERE a.code = :code AND a.id <> :id")
+    boolean existsByCodeAndNotId(@Param("code") String code, @Param("id") Long id);
 }
