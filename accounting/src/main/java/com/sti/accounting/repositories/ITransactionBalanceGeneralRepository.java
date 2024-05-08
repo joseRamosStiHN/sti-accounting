ca881cdca8a6cbda1b5d8eab30ca798c011e914a
Jose Manuel Ramos Morel <jramos@stiglobals.com>
2024-05-08 15:44:31 -0600

    add repository decorator to ITransactionBalanceGeneralRepository

--- accounting/src/main/java/com/sti/accounting/repositories/ITransactionBalanceGeneralRepository.java
+++ accounting/src/main/java/com/sti/accounting/repositories/ITransactionBalanceGeneralRepository.java
@@ -2,9 +2,10 @@ package com.sti.accounting.repositories;
 
 import com.sti.accounting.entities.TransactionBalanceGeneralEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 
-
+@Repository
 public interface ITransactionBalanceGeneralRepository extends CrudRepository<TransactionBalanceGeneralEntity, Long> {
 
 
