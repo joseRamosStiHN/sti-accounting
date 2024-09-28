5385a350ee9e75507bd7fcb70e6739be162f064e
Lcaceressti97 <lcaceres@stiglobals.com>
2024-09-27 18:14:27 -0600

    Cambios Balance General

--- accounting/src/main/java/com/sti/accounting/services/GeneralBalanceService.java
+++ accounting/src/main/java/com/sti/accounting/services/GeneralBalanceService.java
@@ -31,6 +31,10 @@ public class GeneralBalanceService {
 
         List<AccountEntity> accounts = iAccountRepository.findAll();
 
+        accounts = accounts.stream()
+                .filter(account -> account.getAccountCategory().getName().equalsIgnoreCase("Balance General"))
+                .toList();
+
         List<GeneralBalanceResponse> response = new ArrayList<>();
 
         for (AccountEntity account : accounts) {
@@ -62,21 +66,40 @@ public class GeneralBalanceService {
     }
 
     private String getCategory(AccountEntity account) {
-        String parentName = account.getParent() != null ? account.getParent().getDescription() : null;
-        if (parentName != null) {
-            if (isMainCategory(parentName)) {
-                return parentName;
-            } else {
-                return getCategory(account.getParent());
+        if (account.getParent() == null) {
+            if (account.getDescription().toUpperCase().contains("ACTIVO")) {
+                return "Activo";
+            } else if (account.getDescription().toUpperCase().contains("PASIVO")) {
+                return "Pasivo";
+            } else if (account.getDescription().toUpperCase().contains("PATRIMONIO")) {
+                return "Patrimonio";
             }
         } else {
-            return "ACTIVO";
+            String parentName = account.getParent().getDescription();
+            if (parentName.toUpperCase().contains("ACTIVO")) {
+                return "Activo";
+            } else if (parentName.toUpperCase().contains("PASIVO")) {
+                return "Pasivo";
+            } else if (parentName.toUpperCase().contains("PATRIMONIO")) {
+                return "Patrimonio";
+            } else {
+                AccountEntity parent = account.getParent();
+                while (parent != null) {
+                    if (parent.getDescription().toUpperCase().contains("ACTIVO")) {
+                        return "Activo";
+                    } else if (parent.getDescription().toUpperCase().contains("PASIVO")) {
+                        return "Pasivo";
+                    } else if (parent.getDescription().toUpperCase().contains("PATRIMONIO")) {
+                        return "Patrimonio";
+                    }
+                    parent = parent.getParent();
+                }
+                return "ACTIVO"; // Valor por defecto
+            }
         }
+        return "";
     }
 
-    private boolean isMainCategory(String category) {
-        return category.equalsIgnoreCase("ACTIVO") || category.equalsIgnoreCase("PASIVO") || category.equalsIgnoreCase("PATRIMONIO");
-    }
 
     private BigDecimal getBalance(ControlAccountBalancesEntity sumViewEntity) {
         BigDecimal debit = sumViewEntity.getDebit() != null ? new BigDecimal(sumViewEntity.getDebit()) : BigDecimal.ZERO;
