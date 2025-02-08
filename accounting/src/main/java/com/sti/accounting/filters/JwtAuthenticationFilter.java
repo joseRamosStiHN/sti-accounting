2a00b448e8279968aca22bf9428db8ffab44f35c
Jose Manuel Ramos Morel <jramos@stiglobals.com>
2025-02-08 17:52:23 -0600

    refactoring Filter to handler better different scenarios with header and cookie that contain JWT

--- accounting/src/main/java/com/sti/accounting/filters/JwtAuthenticationFilter.java
+++ accounting/src/main/java/com/sti/accounting/filters/JwtAuthenticationFilter.java
@@ -1,23 +1,29 @@
 package com.sti.accounting.filters;
 
+import com.sti.accounting.core.CustomUserDetails;
+import com.sti.accounting.core.SecurityUserDto;
 import com.sti.accounting.services.JwtService;
-import com.sti.accounting.utils.TenantContext;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
 
     private final JwtService jwtService;
 
@@ -31,40 +37,73 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             HttpServletResponse response,
             FilterChain filterChain
     ) throws ServletException, IOException {
-        try {
-            String tenantId = request.getHeader("tenantId");
 
-            if (tenantId != null && !tenantId.isEmpty()) {
-                TenantContext.setCurrentTenant(tenantId);
-            }
-
-            final String token = extractTokenFromCookie(request);
-
-            if (token == null) {
-                filterChain.doFilter(request, response);
-                return;
-            }
-
-            final String username = jwtService.extractUsername(token);
-
-            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                if (jwtService.isTokenValid(token)) {
-                    UserDetails userDetails = jwtService.getUserDetails(token);
-                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                            userDetails,
-                            null,
-                            userDetails.getAuthorities()
-                    );
-                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                    SecurityContextHolder.getContext().setAuthentication(authToken);
-                }
-            }
-
-            filterChain.doFilter(request, response);
-        } finally {
-            // Limpiar el contexto del tenant después de cada solicitud
-            // TenantContext.clear();
+        String tenantId = request.getHeader("Tenantid");
+        String token = extractTokenFromCookie(request);
+        // tenantId not exist return with unauthorized
+        if (tenantId == null || tenantId.isEmpty()) {
+            logger.info("tenantId not exist");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+            response.getWriter().write("{\"error\": \"Error el TenantId no encontrado\"}");
+            response.getWriter().flush();
+            response.getWriter().close();
+            return;
         }
+        // if token is null or empty return with unauthorized
+        if (token == null || token.isEmpty()) {
+            logger.info("token not exist");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+            response.getWriter().write("{\"error\": \"Error el token no proved\"}");
+            response.getWriter().flush();
+            response.getWriter().close();
+            return;
+        }
+        // set tenantId to context TODO: revisar esto
+       // TenantContext.setCurrentTenant(tenantId);
+
+        // validate token
+        if (!jwtService.isTokenValid(token)) {
+            logger.info("token expired");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+            response.getWriter().write("{\"error\": \"Error el token a expirado\"}");
+            response.getWriter().flush();
+            response.getWriter().close();
+            return;
+        }
+
+
+        try{
+            SecurityUserDto userDetails = jwtService.getUserDetails(token);
+
+            // Global roles of user
+            List<SimpleGrantedAuthority> authorities = userDetails.getGlobalRoles().stream()
+                    .map(role -> new SimpleGrantedAuthority(role.getName()))
+                    .toList();
+            // create custom user
+            CustomUserDetails customUserDetails = new CustomUserDetails(userDetails,tenantId, authorities);
+            //create SecurityContextHolder
+
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                    customUserDetails,
+                    null,
+                    customUserDetails.getAuthorities()
+            );
+            SecurityContextHolder.getContext().setAuthentication(authToken);
+        } catch (RuntimeException e) {
+            logger.error("Error al autenticar usuario desde token: {}", e.getMessage());
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.setContentType("application/json");
+            response.getWriter().write("{\"error\": \"Error al autenticar usuario desde token\"}");
+            response.getWriter().flush();
+            response.getWriter().close();
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+
     }
 
     private String extractTokenFromCookie(HttpServletRequest request) {
